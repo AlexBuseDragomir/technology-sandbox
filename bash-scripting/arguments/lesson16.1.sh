cfee175ec2f552b1d9cbe07ad4e64dcf95123665
AlexBuseDragomir <alexandru.buse.dragomir@gmail.com>
2026-02-05 12:26:54 +0200

    Small script update

--- bash-scripting/arguments/lesson16.1.sh
+++ bash-scripting/arguments/lesson16.1.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# When you're passing only one argument
 echo "You entered the argument: $1"
+
+# When you're passing multiple arguments
+echo "You entered the arguments: $1, $2, $3 and $4"
